d86b399cf80b66c8517387b56b590a2a9291aace
viku135790 <viku135790@gmail.com>
2022-11-30 10:49:12 +0530

    Add files via upload

---
+++ Map/Hash_Map.java
@@ -0,0 +1,25 @@
+package Map;
+/*if any program is wrong then report on (viku135790@gmail.com);
+* Or u can mail your question (viku135790@gmail.com) */
+import java.util.HashMap;
+
+
+public class Hash_Map {
+	public static void main(String[] args) {
+		
+		HashMap hm=new HashMap();
+		hm.put(2, "Vimal");
+		hm.put(5, "Piyush");
+		hm.put(3, "Ashish");
+		hm.put(4, "Anshul");
+		hm.put("vikas", 23);
+		hm.put(6, "Piyush");
+		hm.put(2, "Pankaj");//value of 2 will be updated
+		
+		System.out.println(hm);
+		
+		//Retrive only in order
+		//add in sequence
+	}
+
+}
